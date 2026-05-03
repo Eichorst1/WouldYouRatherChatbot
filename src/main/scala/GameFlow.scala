d2e1f5cdf5ab33a5f7325641f4789cff6a4aa4e3
Eichorst1 <mmmohamed.elkersh909@gmail.com>
2026-05-03 18:50:30 +0300

    GameFlow.scala

--- src/main/scala/GameFlow.scala
+++ src/main/scala/GameFlow.scala
@@ -18,5 +18,12 @@ object GameFlow {
        |Type A or B to answer.
      """.stripMargin
   }
+  
+def askQuestion(question: WyrQuestion, state: ConversationState): (String, ConversationState) = {
+    val newState = state.copy(currentQuestion = Some(question))
+    (formatQuestion(question), newState)
+  }
 
+  def chooseNextQuestion(data: List[WyrQuestion], alreadyAsked: Set[Int]): Option[WyrQuestion] = {
+    data.find(question => !alreadyAsked.contains(question.id))
 }
