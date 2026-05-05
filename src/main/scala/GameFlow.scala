73429846cf6a4839ca4e493583a57a8986910303
23-101060 <23-101060@students.eui.edu.eg>
2026-05-05 18:42:14 +0300

    Update GameFlow.scala

--- src/main/scala/GameFlow.scala
+++ src/main/scala/GameFlow.scala
@@ -27,3 +27,51 @@ def askQuestion(question: WyrQuestion, state: ConversationState): (String, Conve
   def chooseNextQuestion(data: List[WyrQuestion], alreadyAsked: Set[Int]): Option[WyrQuestion] = {
     data.find(question => !alreadyAsked.contains(question.id))
 }
+def getChoiceReaction(
+    choice: String,
+    question: WyrQuestion
+): String = {
+
+  choice match {
+
+    case "A" =>
+      s"You chose: ${question.optionA}"
+
+    case "B" =>
+      s"You chose: ${question.optionB}"
+
+    case _ =>
+      "Invalid choice."
+  }
+}
+
+def handleAnswer(choice: String,state: ConversationState): (String, ConversationState) = {
+   state.currentQuestion match {
+     case None =>
+     ("No active question.", state)
+
+    case Some(question) =>
+     val reaction =
+        getChoiceReaction(choice, question)
+
+      val currentCount =
+        state.answerStats.getOrElse(choice, 0)
+
+      val newStats =
+        state.answerStats + (choice -> (currentCount + 1))
+
+      val newState =
+        state.copy(
+          alreadyAsked =
+            state.alreadyAsked + question.id,
+
+          currentQuestion = None,
+
+          answerStats = newStats,
+
+          roundsPlayed = state.roundsPlayed + 1
+        )
+
+      (reaction, newState)
+  }
+}
